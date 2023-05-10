d981179da47f851bcb1017ea9f7a8dfda3da9f3e
Young-Yoii <fhqht4545@gmail.com>
2023-05-10 16:45:58 +0900

    drag & drop 구현

--- dragndrop/index.js
+++ dragndrop/index.js
@@ -16,60 +16,31 @@ const setContents = async() => {
     main.innerHTML = str;
 
     let currentItem;
-    let currentItemIndex;
-    // const draggables = document.querySelectorAll(".draggable");
-    // draggables.forEach(draggable => {
-    //     draggable.addEventListener("dragstart", (e) => {
-    //       draggable.classList.add("dragging");
-    //     });
-    //     draggable.addEventListener("dragend", () => {
-    //         draggable.classList.remove("dragging");
-    //     });
-    // });
-
     main.addEventListener("dragstart", (e) => {
-        e.target.classList.contains("draggable") ? currentItem = e.target : currentItem = e.target.parentElement
+        // div 안의 요소가 잡히는 것 방지
+        e.target.classList.contains("draggable") ? currentItem = e.target : currentItem = e.target.parentElement;
         currentItem.classList.add("dragging");
-        const listArr = [...currentItem.parentElement.children];
-        currentItemIndex = listArr.indexOf(currentItem);
     });
-     main.addEventListener("drop", e => {
-        e.preventDefault();
-        const draggable = document.querySelector(".dragging");
-        draggable.classList.remove("dragging");
-        
-     })
     main.addEventListener("dragover", e => {
         e.preventDefault();
         let currentDropItem;
-        e.target.classList.contains("draggable") ? currentDropItem = e.target : currentDropItem = e.target.parentElement
-        // const currentDropItem = e.target;
-        const draggable = document.querySelector(".dragging");
-        // console.log(currentItem)
-        console.log(currentItemIndex)
-        console.log(currentDropItem)
-        console.log(currentItem.getBoundingClientRect())
-        console.log(currentDropItem.getBoundingClientRect())
+        e.target.classList.contains("draggable") || e.target.classList.contains("main") ? currentDropItem = e.target : currentDropItem = e.target.parentElement;
         const listArr = [...currentItem.parentElement.children];
-        const a = currentItem.getBoundingClientRect().left
-        const b = currentDropItem.getBoundingClientRect().left
-        const c = currentItem.getBoundingClientRect().top
-        const d = currentDropItem.getBoundingClientRect().top
+        const curItemLeft = currentItem.getBoundingClientRect().left;
+        const curDropItemLeft = currentDropItem.getBoundingClientRect().left;
+        const curItemTop = currentItem.getBoundingClientRect().top;
+        const curDropItemTop = currentDropItem.getBoundingClientRect().top;
+
         const dropItemIndex = listArr.indexOf(currentDropItem);
-        console.log(dropItemIndex)
-        if(dropItemIndex === -1) return
-        // if (currentItemIndex < dropItemIndex) {
-            a >= b && c>=d? currentDropItem.before(currentItem) : currentDropItem.after(currentItem)
-        // }
-        // else {
-        //     currentDropItem.before(currentItem);
-        // }
 
-        // 1->4 after 가야하는데 before
-        // 7 -> 3 before 가야하는데 after
+        //main 부분이 잡혔을때 방지 --> 개선필요
+        if(dropItemIndex === -1) return;
+        curItemLeft >= curDropItemLeft && curItemTop >= curDropItemTop ? currentDropItem.before(currentItem) : currentDropItem.after(currentItem);
     })
-    
-    
-
+    main.addEventListener("drop", e => {
+        e.preventDefault();
+        const draggable = document.querySelector(".dragging");
+        draggable.classList.remove("dragging");
+    })
 }
-setContents()
+setContents();
