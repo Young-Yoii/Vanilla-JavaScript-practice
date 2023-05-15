7b59e36b1c59da1a25845193285cfbfde1afd41f
Young-Yoii <fhqht4545@gmail.com>
2023-05-15 16:20:30 +0900

    todolist 구현완료

--- todolist/index.js
+++ todolist/index.js
@@ -10,7 +10,6 @@ const todoStartDate = document.getElementById("todoStartDate");
 const todoEndDate = document.getElementById("todoEndDate");
 const setBtn = document.getElementById("setBtn");
 const modal = document.getElementById("modal");
-const ratingBox = document.getElementById("ratingBox");
 const stars = document.querySelectorAll('.star')
 
 const date = new Date().toISOString().slice(0, 10);
@@ -19,32 +18,33 @@ let rating =  setClick.filter(Boolean).length;
 const setView = (setClick) => {
     setClick.forEach((i, index) => i ? stars[index].classList.add("clicked") : stars[index].classList.remove("clicked"));
 };
-setView(setClick)
-dateInput.forEach(item => {
-    item.addEventListener("input", (e) => {
-        e.target.setAttribute("value", e.target.value);
-        e.target.classList.remove("date");
-        e.target === todoStartDate && todoEndDate.setAttribute("min", e.target.value);
-    })
-})
+setView(setClick);
+
+dateInput.forEach(item => {item.addEventListener("input", (e) => {
+    e.target.dataset.placeholder = e.target.value;
+   if(e.target === todoStartDate){
+    todoEndDate.setAttribute("min", e.target.value);
+    if(e.target.value > todoEndDate.dataset.placeholder){
+        todoEndDate.dataset.placeholder = e.target.value;
+    }
+   }
+})});
 setBtn.addEventListener("click", () => {
-    modal.classList.add("show")
-})
+    modal.classList.add("show");
+});
 const showRating = (rating) => {
     let clickStates = [...setClick];
     return setClick = clickStates.map((i, idx) => clickStates[idx] = idx <= rating-1 ? true : false);
-}
+};
 const handle = e => {
     const target = e.target;
     let clickStates = [...setClick];
     setClick = clickStates.map((i, idx) => clickStates[idx] = idx <= parseInt(target.dataset.index) ? true : false);
     rating = setClick.filter(Boolean).length;
-    console.log(setClick, target.dataset.index, setClick[parseInt(target.dataset.index)], stars[parseInt(target.dataset.index)]);
     setView(setClick);
-}
+};
 
-stars.forEach((i) => {i.addEventListener('click', (e) => {handle(e)})})
-
+stars.forEach((i) => i.addEventListener('click',handle));
 
 const app = async() => {
     let arr = [];
@@ -55,9 +55,7 @@ const app = async() => {
         let content = `
             <div class="tit">
                 <h4>${item.title}</h4>
-                <input type="text" id="titInput" value="${item.title}" class="tit-input">
                 ${item.cont && `<p>${item.cont}</p>`}
-                <input type="text" id="contInput" value="${item.cont}" class="cont-input">
             </div>
             <div>
                 <div class="btn">
@@ -67,30 +65,35 @@ const app = async() => {
                 </div>
             `
         let rating = '';
-        showRating(item.rating).forEach((i, index)=> rating += `<span class=${i && 'clicked'} data-index=${index}>★</span>`)
+        showRating(item.rating).forEach((i, index)=> rating += `<span class=${i && 'clicked'} data-index=${index}>★</span>`);
         item.isComplete === false && item.start === date &&
         todoList.insertAdjacentHTML('beforeend', `
             <li>
                 ${content}
-                <div class="date">~ ${item.end}</div>
-                ${item.rating && `<div id="ratingView" class="rating-view" value=${item.id}>${rating}</div>`}
+                <div class="date">~ ${item.end}<span class="start-none">${item.start}</span><span class="end-none">${item.end}</span></div>
+                ${item.rating && `<div id="ratingView" class="rating-view" value=${item.rating}>${rating}</div>`}
                 </div>
             </li>
-        `)
-        
+        `);
         item.isComplete === false && item.end < date &&
         undoList.insertAdjacentHTML('beforeend', `
             <li>
                 ${content}
-                <div class="date">~ ${item.end}</div>
+                <div class="date">~ ${item.end}
+                    <span class="start-none">${item.start}</span><span class="end-none">${item.end}</span>
+                </div>
+                <div id="ratingView" class="rating-view none" value=${item.rating}>${rating}</div>
                 </div>
             </li>
-        `)
+        `);
         item.isComplete === false && item.start > date &&
         havetodoList.insertAdjacentHTML('beforeend', `
             <li>
                 ${content}
-                <div class="date">${item.start} ~</div>
+                <div class="date">${item.start} ~
+                    <span class="start-none">${item.start}</span><span class="end-none">${item.end}</span>
+                </div>
+                <div id="ratingView" class="rating-view none" value=${item.rating}>${rating}</div>
                 </div>
             </li>
         `)
@@ -98,40 +101,59 @@ const app = async() => {
     const $deleteBtn = document.querySelectorAll(".delete-btn");
     const $completeBtn = document.querySelectorAll(".complete-btn");
     const $updateBtn = document.querySelectorAll(".update-btn");
-
-
     
-    $deleteBtn.forEach(item => {item.addEventListener("click", async(e) => {
+    $deleteBtn.forEach(item => item.addEventListener("click", async(e) => {
         const target = parseInt(e.target.id);
         axios.delete(`http://localhost:3000/todolist/${target}`, {
             data: {
               id: target,
               },
            })
-    })})
-    $completeBtn.forEach(item => {item.addEventListener("click", async(e) => {
+    }));
+    $completeBtn.forEach(item => item.addEventListener("click", async(e) => {
         const target = parseInt(e.target.id);
         axios.patch(`http://localhost:3000/todolist/${target}`, {"isComplete": true})
-    })})
-    
-}
+    }));
+    $updateBtn.forEach(item => item.addEventListener("click", async(e) => {
+        const targetId = parseInt(e.target.id);
+        const targetClosest = e.target.closest('li');
+        submitBtn.innerHTML = "수정하기";
+        submitBtn.setAttribute("value", targetId);
 
+        // 모달창 값 넣어주기(조금 더 효율적인 방법 고안해보기)
+        todoTit.value = targetClosest.querySelector('h4').textContent;
+        targetClosest.querySelector('p') ? todoCont.value = targetClosest.querySelector('p').textContent : todoCont.value="";
+        todoStartDate.dataset.placeholder = targetClosest.querySelector('.start-none').textContent;
+        todoEndDate.dataset.placeholder = targetClosest.querySelector('.end-none').textContent;
+        const rating = targetClosest.querySelector('.rating-view').getAttribute('value');
+        showRating(rating);
+        setView(setClick);
 
-const postData = async() => {
+        modal.classList.add("show");
+    }));
+};
+
+const postData = async(e) => {
    const title = todoTit.value;
    const cont = todoCont.value;
-   const start = todoStartDate.value;
-   const end = todoEndDate.value;
-
+   const start = todoStartDate.dataset.placeholder;
+   const end = todoEndDate.dataset.placeholder;
+   const target = parseInt(e.target.value);
+    
    if(title === '' || start === '' || end === '') {
         if(title === '') {alert("제목을 입력해주세요"); todoTit.focus();}
         else if(start === '') {alert("시작 날짜를 입력해주세요"); todoStartDate.focus();}
         else {alert("종료날짜를 입력해주세요"); todoEndDate.focus();}
     return;
+   };
+
+  if(!target){
+    axios.post(`http://localhost:3000/todolist`, {title, cont, start, end, "isComplete": false, rating});
+  }
+  else {
+    axios.patch(`http://localhost:3000/todolist/${target}`, {title, cont, start, end, "isComplete": false, rating});
    }
+};
+submitBtn.addEventListener("click", postData);
 
-    axios.post(`http://localhost:3000/todolist`, {title, cont, start, end, "isComplete": false, rating})
-}
-submitBtn.addEventListener("click", postData)
-
-app()
+app();
