78a8bacda537633e66bf0112c64f9bcaaf306e09
Young-Yoii <fhqht4545@gmail.com>
2023-05-24 15:31:57 +0900

    localStorage 저장, 불러오기

--- stickynote/index.js
+++ stickynote/index.js
@@ -1,53 +1,65 @@
 const container = document.getElementById('container');
 const addMemoBtn = document.getElementById('addMemoBtn');
 
-let arr = [];
-let optionArr = ['#FFF8D1', '#FFDBAB', '#FDD4E2', '#D8DBFF', '#b2ddff', '#b3ece6']
+let optionArr = [{color: ['#FFF8D1', '#FFDBAB', '#FDD4E2', '#D8DBFF', '#b2ddff', '#b3ece6'] , 
+                  fontSize: ['20px', '30px', '35px', '40px', '45px', '50px', '55px']}]
+const render = () => {
+const localData = localStorage.getItem('memo');
+let arr =  JSON.parse(localData) ? JSON.parse(localData) : [];
+
 const addMemo = () => {
-    const memo = document.createElement('div');
-    memo.setAttribute('id', 'memo');
-    memo.setAttribute('class', 'memo');
-    const extendBtn = document.createElement('div');
-    extendBtn.setAttribute('id', 'extendBtn');
-    extendBtn.setAttribute('class', 'memo__extend');
-    let optionStr= '';
-    optionArr.forEach((option,index) => optionStr += `<label id="colorLable" class="color-item__${index+1}"><input type="radio" value=${option} name="bg-color"><span class="checkmaker">
-    </span></label>`);
-    let basicStr = `
-        <div class="memo__container" id="memoContainer">
-            <div id="memoHeader" class="memo__header">
-                <h4>📝메모</h4>
-                <div class="memo__btn">
-                    <button id="fullScreenBtn" class="memo__btn--fullscreen">🖥️</button>
-                    <button id="deleteBtn" class="memo__btn--del">❌</button>
-                </div>
+    let colorOptionStr= '';
+    let fontOptionStr= '';
+    let flag = arr.length <= container.childElementCount ? false : true;
+    (optionArr[0].color).forEach((option,index) => colorOptionStr += 
+    `<label id="colorLable" class="color-item__${index+1}">
+        <input type="radio" value=${option} name="bg-color">
+        <span class="checkmaker"></span>
+    </label>`);
+    (optionArr[0].fontSize).forEach(option => fontOptionStr += 
+    `<li class="drop-item">${option}</li>`);
+    let containStr = `
+    <div class="memo__container" id="memoContainer">
+        <div id="memoHeader" class="memo__header">
+            <h4>📝메모</h4>
+            <div class="memo__btn">
+                <button id="fullScreenBtn" class="memo__btn--fullscreen">🖥️</button>
+                <button id="deleteBtn" class="memo__btn--del">❌</button>
             </div>
-            <div id="memoBody" class="memo__body">
-                <textarea id="memoText" placeholder="내용을 입력하세요" class="memo__text"></textarea>
+        </div>
+    <div id="memoBody" class="memo__body">`
+    let bodyStr = `<ul id="dropMenu" class="drop-menu">${fontOptionStr}</ul>
+        </div>
+        <div class="memo__color">${colorOptionStr}</div>
+    </div></div></div>  
+    <div id="extendBtn" class="memo__extend"></div>`
+    if(flag) {
+        arr.forEach(i => container.insertAdjacentHTML('beforeend',  `
+        <div id="memo" class="memo" style="width: ${i.width}; height: ${i.height}; top: ${i.top}; left:${i.left};">
+            ${containStr}
+                <textarea id="memoText" placeholder="내용을 입력하세요" class="memo__text" style="background: ${i.backgroundColor};font-size: ${i.fontSize};">${i.content}</textarea>
                 <div id="memoOption" class="memo__container--option">
                     <div id="memoFontDrop" class="memo__font">
-                        <button id="fontBtn" class="font-btn">40px</button>
-                        <ul id="dropMenu" class="drop-menu">
-                            <li class="drop-item">20px</li>
-                            <li class="drop-item">30px</li>
-                            <li class="drop-item">35px</li>
-                            <li class="drop-item">40px</li>
-                            <li class="drop-item">45px</li>
-                            <li class="drop-item">50px</li>
-                            <li class="drop-item">55px</li>
-                        </ul>
-                    </div>
-                   <div class="memo__color">${optionStr}</div>
-                </div>
-            </div>
-        </div>  
-    `
-    
-    memo.innerHTML = basicStr;
-    container.appendChild(memo);
-    memo.appendChild(extendBtn);
-    positionSticky(memo);
-    
+                        <button id="fontBtn" class="font-btn">${i.fontSize}</button>
+                    ${bodyStr}
+        </div>`));
+    }else {
+        const memo = document.createElement('div');
+        memo.setAttribute('id', 'memo');
+        memo.setAttribute('class', 'memo');
+        let str = `
+            ${containStr}
+             <textarea id="memoText" placeholder="내용을 입력하세요" class="memo__text" style="background: #FFF8D1;font-size: 40px;"></textarea>
+             <div id="memoOption" class="memo__container--option">
+                <div id="memoFontDrop" class="memo__font">
+                    <button id="fontBtn" class="font-btn">40px</button>
+                    ${bodyStr}
+        `
+        memo.innerHTML = str;
+        container.append(memo);
+        positionSticky(memo);
+    };
+
     const allExtendBtn = document.querySelectorAll("#extendBtn");
     const allMemo = document.querySelectorAll("#memoHeader");
     extendMemo(allExtendBtn);
@@ -66,7 +78,7 @@ const positionSticky = (memo) => {
       (-100 + Math.round(Math.random() * 50)) +
       'px'; 
     memo.style.width = '500px';
-    memo.style.height = '300px'; 
+    memo.style.height = '300px';
 }
 
 let dragTarget;
@@ -82,12 +94,13 @@ const extendMemo = (extendBtn) => {
     extendBtn.forEach(item => {
         item.addEventListener("mousedown", (e) => {
         e.stopPropagation();
+        const memo = e.target.closest("#memo");
+        const find = arr.findIndex(item => item.content === memo.querySelector('.memo__text').value);
         isExtending = true;
         let prevX = e.screenX;
         let prevY = e.screenY;  
         DEFAULT_W = e.target.parentNode.clientWidth; 
         DEFAULT_H  = e.target.parentNode.clientHeight; 
-        
         const mouseMoveHandler = (e) => {
             const X = e.screenX - prevX - 40;
             const Y = e.screenY - prevY - 40;
@@ -100,15 +113,16 @@ const extendMemo = (extendBtn) => {
             }
             return;
         };
-
         const mouseUpHandler = () => {
             document.removeEventListener('mousemove', mouseMoveHandler);
             isExtending = true;
             isDragging = false;
             document.body.style.cursor = "default";
             e.target.classList.remove("show");
+            arr[find].width = memo.style.width;
+            arr[find].height = memo.style.height;
+            localStorage.setItem('memo',JSON.stringify(arr));
           };
-    
           document.addEventListener('mousemove', mouseMoveHandler);
           document.addEventListener('mouseup', mouseUpHandler, { once: true });
     })}
@@ -117,46 +131,68 @@ const extendMemo = (extendBtn) => {
 //드래그앤드롭
 const dragMemo = (allMemo) => {
     allMemo.forEach(item => item.addEventListener('mousedown', e => {
-        dragTarget = item.parentNode.parentNode;
+        const memo = e.target.closest("#memo");
+        const find = arr.findIndex(item => item.content === memo.querySelector('.memo__text').value);
+        console.log(find, memo,dragTarget)
         lastOffsetX = e.offsetX;
         lastOffsetY = e.offsetY;
         isDragging = true;
         const drag = (e) => {
             e.preventDefault();
             if (!isDragging) return;
-            dragTarget.style.left = e.clientX - lastOffsetX - 20 + 'px';
-            dragTarget.style.top = e.clientY - lastOffsetY - 20 + 'px';
+            memo.style.left = e.clientX - lastOffsetX - 20 + 'px';
+            memo.style.top = e.clientY - lastOffsetY - 20 + 'px';
         }
         container.addEventListener('mousemove', drag);
-        container.addEventListener('mouseup', () => isDragging = false);
+        container.addEventListener('mouseup', () => {
+            isDragging = false;
+            arr[find].left = memo.style.left;
+            arr[find].top = memo.style.top;
+            localStorage.setItem('memo',JSON.stringify(arr));
+        });
     }));
 }
 
-const data = (text, height, width) => {
-    let obj = {
-        "text": text,
-        "height" : height,
-        "width": width
-    }
-    arr.push(obj)
-    console.log(arr)
-}
-
 addMemoBtn.addEventListener("click", addMemo);
-// forEach 메모리 낭비 ==> 이벤트 위임방식으로 변경
+/// forEach 메모리 낭비 ==> 이벤트 위임방식으로 변경
 container.addEventListener("click", (e) => {
-    const target = e.target;
-    const div = e.target.closest('div');
+    let target = e.target;
+    if(target.className === 'container' && target.className === 'memo__extend' ){return;}
     const memo = e.target.closest("#memo");
-    console.log(e.target)
+    const find = arr.findIndex(item => item.content === memo.querySelector('.memo__text').value);
+    if(find === -1) return;
+    // 메모 내용
+    if(target.className === 'memo__text'){
+        target.addEventListener("change", (e) => {
+            let flag = arr.length >= container.childElementCount ? true : false;
+            //내용 업데이트
+            if(flag){
+                let value = ''
+                value = target.value;
+                arr[find].content = value;
+            }else{ //새 데이터 추가
+                let data = {'content': target.value, 
+                'width': memo.style.width, 
+                'height': memo.style.top, 
+                'top':  memo.style.top, 
+                'left':  memo.style.left, 
+                'fontSize': target.style.fontSize, 
+                'backgroundColor': target.style.background}
+                arr.push(data);
+            }
+            localStorage.setItem('memo',JSON.stringify(arr))
+        })
+    }
     // 폰트 사이즈 적용
     if(target.className === 'font-btn'){
         target.nextElementSibling.classList.toggle("active")
     }
     if(target.className === 'drop-item'){
         const fontSize = target.textContent;
-        div.firstElementChild.textContent = fontSize;
-        div.parentNode.previousElementSibling.style.fontSize = fontSize;
+        memo.querySelector('.font-btn').textContent = fontSize;
+        memo.querySelector('.memo__text').style.fontSize = fontSize;
+        arr[find].fontSize = target.textContent;
+        localStorage.setItem('memo',JSON.stringify(arr));
     }
     // 최대화
     if(target.className === 'memo__btn--fullscreen'){
@@ -186,11 +222,18 @@ container.addEventListener("click", (e) => {
         }
     }
     if(target.getAttribute('id') === 'colorLable' || target.className === 'checkmaker'){
-        let value;
-        target.getAttribute('id') === 'colorLable' ? value = target.firstElementChild.value : value = target.previousElementSibling.value;
-        div.parentNode.previousElementSibling.style.background = value;
+        target.className === 'checkmaker' ? target = target.parentNode : target
+        memo.querySelector('.memo__text').style.background = target.firstElementChild.value;
+        arr[find].backgroundColor = target.firstElementChild.value;
+        localStorage.setItem('memo',JSON.stringify(arr));
     }
+    //삭제
     if(target.className === 'memo__btn--del'){
         memo.remove();
+        const filter = arr.filter(item => item.content !== memo.querySelector('.memo__text').value);
+        arr = filter;
+        localStorage.setItem('memo',JSON.stringify(arr))
     }
-})
+    return;
+})}
+render();
